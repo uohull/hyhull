f18f2d86ffd8dc7ff41a396953a2be0b70487f27
Simon Lamb <s.lamb@hull.ac.uk>
2013-11-07 11:45:33 +0000

    hyhull-54 - Added functionality to display metadata tabs in red when validation errors occur.

--- app/assets/javascripts/metadata.js
+++ app/assets/javascripts/metadata.js
@@ -3,3 +3,22 @@ $(function() {
     dateFormat: "yy-mm-dd"
   });
 });
+
+/* 
+ * This method is used to change the colour of the Metadata tab pane links if validation errors are present in the form
+ * Class 'error-link' is adding to the a-link and required * symbol is prepended.
+ * metadata-edit-nav id is required on the nav tab-pane
+ */
+$( document ).ready(function() {
+  $('#metadata-edit-nav > li').each( function() {
+    var tab_pane_id = $(this).children().attr('href');     
+    var error_fields =  $(tab_pane_id).find('.error');
+
+    if (error_fields.size() > 0) {
+      var a_element = $(this).children('a')
+      // Add the error-link class and * abbrievation
+      $(a_element).addClass('error-link')
+      $(a_element).prepend('<abbr title="required">*</abbr>&nbsp;')
+    }
+  });
+});
