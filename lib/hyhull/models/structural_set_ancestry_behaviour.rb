d3d75248a564a29bc317e67c5eab3227965e88a9
Simon Lamb <s.lamb@hull.ac.uk>
2013-12-13 12:48:52 +0000

    Updated StructuralSetAncestryBehaviour to retrieve the correct title field

--- lib/hyhull/models/structural_set_ancestry_behaviour.rb
+++ lib/hyhull/models/structural_set_ancestry_behaviour.rb
@@ -26,7 +26,7 @@ module Hyhull
 
           def retrieve_set_members pid
             fields = "is_member_of_ssim:info\\:fedora\\/" + pid
-            options = {:fl=>["id", "title_ssm", "is_member_of_ssim", "active_fedora_model_ssi"], :rows=>10000, :sort=>"system_create_dtsi asc" }
+            options = {:fl=>["id", "title_tesim", "is_member_of_ssim", "active_fedora_model_ssi"], :rows=>10000, :sort=>"system_create_dtsi asc" }
             ActiveFedora::SolrService.query(fields,options)
           end
 
