b13d8f2325b02cc6a915946dfa5c75fd9b84daed
richgj700 <rich.garbutt@gmail.com>
2013-12-13 10:26:11 +0000

    Cleanup assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ Hyhull::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  #Add the following blah specific js/cs - add2home functionality
+  config.assets.precompile += ['add2home.js', 'add2home.css', 'stay_standalone.js']
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
