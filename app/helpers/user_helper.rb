86eb01a819120bd07945cd4c01d19154f4d7f6d6
Simon Lamb <s.lamb@hull.ac.uk>
2013-12-24 11:14:12 +0000

    Popular searches text: Revised links, and dynamically displays based upon groups.

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,23 @@
+module UserHelper
+
+  #########################################################
+  #                                                       #
+  # UserHelper                                            #
+  # * Provides user/session helper methods                #
+  #                                                       #
+  #########################################################
+
+  def staff_or_student_user?
+    staff_or_student = false
+    if has_user_authentication_provider? && current_user       
+      staff_or_student = user_member_of_group?(current_user, "student") || user_member_of_group?(current_user, "staff")
+    end
+  end
+
+  private 
+
+  def user_member_of_group?(user, group)
+    return user.groups.include?(group)
+  end
+
+end
