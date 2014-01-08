58e0fe6e783b94de6b72b5c694165cdbc94f42a6
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-08 10:19:33 +0000

    Updates include:-
      - Content creator, content access team, and admin links filtered on homepage
      - New powered by Hydra logo
      - Fixed missing closing a tag in Searches grid
      - Minor grammar issue in Search tile

--- app/helpers/user_role_helper.rb
+++ app/helpers/user_role_helper.rb
@@ -14,6 +14,20 @@ module UserRoleHelper
     end
   end
 
+  def contentAccessTeam_user? 
+    contentAccessTeam_user = false
+    if has_user_authentication_provider? && current_user       
+      contentAccessTeam_user = user_member_of_group?(current_user, "contentAccessTeam")
+    end
+  end
+
+  def admin_user? 
+    admin_user = false
+    if has_user_authentication_provider? && current_user       
+      admin_user = user_member_of_group?(current_user, "admin")
+    end
+  end
+
   private 
 
   def user_member_of_group?(user, group)
