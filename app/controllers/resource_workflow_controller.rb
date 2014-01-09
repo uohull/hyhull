d467f9a779b46c1709e988d7bd0b755b73c5d2cf
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-09 18:47:57 +0000

    Resource workflow transitions now will redirect back to edit page if you have edit permissions

--- app/controllers/resource_workflow_controller.rb
+++ app/controllers/resource_workflow_controller.rb
@@ -1,7 +1,8 @@
 class ResourceWorkflowController < ApplicationController
+  # We manually add the load_and_authorize_resource CanCan method to do and specify it to use the authorisation based on the Object
+  load_and_authorize_resource class: ActiveFedora::Base
 
   def update
-
     id = params[:id]
     object = ActiveFedora::Base.find(id, cast: true)
     state = params[:resource_state]
@@ -11,8 +12,21 @@ class ResourceWorkflowController < ApplicationController
       begin
         object.fire_resource_state_event(state)
         if object.save
+
+          # After the object has saved the permissions have potentially changed, therefore we clear the obj permissions cache 
+          current_ability.cache.clear
+
+          # Can the user still edit resource post queue transition... 
+          user_can_still_edit_resource = can?(:edit, object)
+
           notice = { :notice => "Sucessfully added resource #{object.id} to the #{ object.human_resource_state_name } queue" }
-          redirect_to root_url, notice 
+
+          if user_can_still_edit_resource
+            redirect_to :back, notice 
+          else
+            redirect_to root_url, notice 
+          end
+
         else
           notice = { :alert => "Problems saving the resource to the #{state} queue...</br></br>#{object.errors.full_messages.join("</br>")}".html_safe }
           redirect_to :back, notice 
