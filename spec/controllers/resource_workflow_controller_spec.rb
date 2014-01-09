d4567c5616ba45a16b29d1d3d517934cea66d0c8
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-09 23:06:51 +0000

    Update spec for ResourceWorkflowController

--- spec/controllers/resource_workflow_controller_spec.rb
+++ spec/controllers/resource_workflow_controller_spec.rb
@@ -1,5 +1,6 @@
 # spec/controllers/uketd_object_controller_spec.rb
 require 'spec_helper'
+require 'user_helper'
 
 
 #Test OM Datastream
@@ -19,7 +20,7 @@ class TestOmDatastream < ActiveFedora::OmDatastream
 end
 
 class ResourceWorkflowControllerTestClass < ActiveFedora::Base
-
+  include Hydra::ModelMethods
   include Hyhull::ModelMethods
   include Hyhull::ResourceWorkflowBehaviour
 
@@ -31,18 +32,23 @@ class ResourceWorkflowControllerTestClass < ActiveFedora::Base
   end
 
   has_metadata :name => "test_ds", :label=>"test_ds", type: TestOmDatastream
+  has_metadata name: "rightsMetadata", label: "Rights metadata" , type: Hydra::Datastream::RightsMetadata
   has_attributes :mandatory_field_for_submit_transition, datastream: :test_ds, multiple: false
 
 end
 
 
 describe ResourceWorkflowController do
+  include UserHelper
+
   set_referer
   login_cat  
 
   describe "updating" do   
     before(:each) do
+      cat_user_sign_in
       @test = ResourceWorkflowControllerTestClass.new
+      @test.apply_depositor_metadata(@user.username, @user.email)
       @test.save
       @pid = @test.pid 
     end
@@ -54,6 +60,19 @@ describe ResourceWorkflowController do
         put :update, :id=>@pid, :resource_state => "submit"
         flash[:notice].should == "Sucessfully added resource #{@pid} to the qa queue"
       end
+
+      it "should redirect to the homepage when the transition removes the users edit permissions" do
+        @test.mandatory_field_for_submit_transition = "This has been populated"
+        @test.submit_resource!
+        @test.hide_resource! 
+        # Resource is now in hidden queue...
+        @test.save
+
+        put :update, :id=>@pid, :resource_state => "delete" 
+        response.should redirect_to root_path
+        flash[:notice].should == "Sucessfully added resource #{@pid} to the deleted queue"
+      end
+
     end
     context "invalid update" do
       it "should display the correct flash message for an invalid transition" do
