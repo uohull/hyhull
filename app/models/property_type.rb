c5f6e005990c5ee1322d6eb799b7757d60ef73b5
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-16 14:01:04 +0000

    HYHULL-190 Added Abilities tests, and minor changes to the Property and PropertyType models.  Removed hardcoded confiugurations from models, ModsMetadataBase and hyhull config.

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -1,3 +1,6 @@
+# This ActiveRecord::Base class is used to store the PropertyType for Hyhull
+# PropertyType has a name and description
+# Name is generally used in the Property helper for display dropdown selects
 class PropertyType < ActiveRecord::Base
   
   has_many :properties 
