d012b1c19ec45024091a8a7ff52844b21ab68ad4
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-20 16:50:23 +0000

    hyhull-190 Added parameter to PropertyHelper#select_by_property_type to remove blank option from dropdown list

--- app/helpers/property_helper.rb
+++ app/helpers/property_helper.rb
@@ -2,7 +2,7 @@
 module PropertyHelper
   # Provides a Select dropdown based upon the proptery_type_name 
   # sort_dropdown is an optional (defaults to true) - Will sort by name..
-  def select_by_property_type(form, attribute, property_type_name, sort_dropdown=true)
-    form.input attribute, collection: Property.select_by_property_type_name(property_type_name, sort_dropdown), value_method: "value"
+  def select_by_property_type(form, attribute, property_type_name, sort_dropdown=true, include_blank=true)
+    form.input attribute, collection: Property.select_by_property_type_name(property_type_name, sort_dropdown), value_method: "value", include_blank: include_blank
   end
 end
