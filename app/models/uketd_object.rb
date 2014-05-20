426c6858a94ee3b353af54262efc3d55ae305b3b
Simon Lamb <s.lamb@hull.ac.uk>
2014-05-20 23:36:38 +0100

    Further modifications for DB auth version.
    Removed Hull specific person model and related specs
    Updated db seeds file

--- app/models/uketd_object.rb
+++ app/models/uketd_object.rb
@@ -28,8 +28,8 @@ class UketdObject < ActiveFedora::Base
   has_metadata name: "rightsMetadata", label: "Rights metadata" , type: Hydra::Datastream::RightsMetadata
 
   # UKETD_DC Datastream - Utilises Disseminator on the Fedora instance
-  has_metadata name: "UKETD_DC", label: "UKETD_DC metadata", autocreate: true, control_group: "E", 
-                disseminator: "hull-sDef:uketdObject/getUKETDMetadata", versionable: false, :type => ActiveFedora::OmDatastream
+  #has_metadata name: "UKETD_DC", label: "UKETD_DC metadata", autocreate: true, control_group: "E", 
+  #              disseminator: "hull-sDef:uketdObject/getUKETDMetadata", versionable: false, :type => ActiveFedora::OmDatastream
 
   # Attributes to respective datastream
   #Unique fields
--- db/schema.rb
+++ db/schema.rb
@@ -22,16 +22,6 @@ ActiveRecord::Schema.define(:version => 20131208185212) do
     t.string   "user_type"
   end
 
-  create_table "people", :force => true do |t|
-    t.string "username"
-    t.string "given_name"
-    t.string "family_name"
-    t.string "email_address"
-    t.string "user_type"
-    t.string "department_ou"
-    t.string "faculty_code"
-  end
-
   create_table "properties", :force => true do |t|
     t.string  "name"
     t.string  "value"
