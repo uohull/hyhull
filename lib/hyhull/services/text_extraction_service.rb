bc7ba959940b3efce20f51a5888903eaf0242bb9
Simon Lamb <s.lamb@hull.ac.uk>
2014-07-23 23:33:04 +0100

    Improvement and fix for Text extraction service code.  The service will now respect and use authentication user:pass specified in the URL within solr.yml

--- lib/hyhull/services/text_extraction_service.rb
+++ lib/hyhull/services/text_extraction_service.rb
@@ -4,11 +4,25 @@ module Hyhull
       def self.extract_text(file_content, mime_type)
         begin
           if self.valid_content_types.include? mime_type
-            url = Blacklight.solr_config[:url] ? Blacklight.solr_config[:url] : Blacklight.solr_config["url"] ? Blacklight.solr_config["url"] : Blacklight.solr_config[:fulltext] ? Blacklight.solr_config[:fulltext]["url"] : Blacklight.solr_config[:default]["url"] 
-            uri = URI(url+'/update/extract?&extractOnly=true&wt=ruby&extractFormat=text')
-            req = Net::HTTP.new(uri.host, uri.port)
-            resp = req.post(uri.to_s, file_content, {'Content-type'=>mime_type+';charset=utf-8', "Content-Length"=>"#{file_content.size}" })
-            text = eval(resp.body)[""].strip
+
+            solr_base_url = Blacklight.solr_config[:url] ? Blacklight.solr_config[:url] : Blacklight.solr_config["url"] ? Blacklight.solr_config["url"] : Blacklight.solr_config[:fulltext] ? Blacklight.solr_config[:fulltext]["url"] : Blacklight.solr_config[:default]["url"] 
+            solr_text_extraction_url = "#{solr_base_url}/update/extract?extractOnly=true&wt=ruby&extractFormat=text"
+
+            uri = URI(solr_text_extraction_url)
+
+            http = Net::HTTP.new(uri.host, uri.port)
+
+            # Set username/password if configured in SOLR URL
+            username = uri.user.nil? ? "" : uri.user
+            password = uri.password.nil? ? "" : uri.password
+
+            request = Net::HTTP::Post.new(uri.request_uri, { "Content-type" => "#{mime_type};charset=utf-8", "Content-Length" => "#{file_content.size}" })
+            request.body = file_content
+            request.basic_auth(username, password) unless username.empty? && password.empty?
+
+            response = http.request(request)
+            text = eval(response.body)[""].strip
+
             return text
           else
             raise "The mime_type is not a valid content type for text extraction"
