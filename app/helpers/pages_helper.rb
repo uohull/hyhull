df9120f675c6ae26483ec8afea6222e1dd42dc6a
Simon Lamb <s.lamb@hull.ac.uk>
2014-12-23 15:56:19 +0000

    Added simple helper for display a relevant exam paper link

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+
+ # Returns a Link to the Examination papers through Login, if not a current_user, and a standard link if already signed in as staff/student 
+  def exam_papers_link
+    if current_user.nil?        
+        link_to("Examination papers (Click to sign in)", "resources?f[genre_sim][]=Examination+paper&login=true")
+    else
+       link_to("Examination papers", "resources?f[genre_sim][]=Examination+paper") if staff_or_student_user?
+    end
+  end
+
 end
