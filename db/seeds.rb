27839c01cd591fbf7858b9c966dca7c8f0b62405
richgj700 <rich.garbutt@gmail.com>
2015-04-14 09:17:28 +0100

    Add working saml config for ping federate

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,26 +87,27 @@ unless Rails.env.production?
   # For test lets seed some made up departments and faculities (these need to exist in the Roles DB to be picked up on user login)
 
   # Seed the dep roles
-  [{ name:"IT", description: "IT Department"},
+  [{ name:"064", description: "IT Department"},
   { name:"LLI", description: "LLI Department"},  
   { name:"CompSci", description: "Computer Science"}
   ].each{ |r| Role.create(name: r[:name], description: r[:description], role_type: RoleType.find_or_initialize_by_name("department_ou")) }
 
   # Seed the faculty roles
-  [{ name:"123", description: "IT/LLI Faculty"},
-  { name:"456", description: "Science and Engineering"} 
+  [{ name:"123", description: "IT/LLI Faculty" },
+  { name:"456", description: "Science and Engineering" }, 
+  { name: "ICT", description: "IT Department" }
   ].each{ |r| Role.create(name: r[:name], description: r[:description], role_type: RoleType.find_or_initialize_by_name("faculty_code")) }
 
   # Seed some 'people' into the Person model, in production the Person model is populated from the Portal Person DB
   # These accounts are for test purposes only...
    [{ username: 'admin1',  given_name: 'The', family_name: 'Admin', email_address: 'admin1@example.com', 
-     user_type: 'staff', department_ou: 'IT', faculty_code: '123'},
+     user_type: 'staff', department_ou: '064', faculty_code: '123'},
      { username: 'contentaccessteam1',  given_name: 'content', family_name: 'team', email_address: 'contentAccessTeam1@example.com', 
-     user_type: 'contentAccessTeam', department_ou: 'IT', faculty_code: '123'},
+     user_type: 'contentAccessTeam', department_ou: '064', faculty_code: '123'},
      { username: 'contentcreator1',  given_name: 'content', family_name: 'creator', email_address: 'contentCreator1@example.com', 
-     user_type: 'staff', department_ou: 'IT', faculty_code: '123'},
+     user_type: 'staff', department_ou: '064', faculty_code: '123'},
      { username: 'staff1',  given_name: 'staff', family_name: 'user', email_address: 'staff1@example.com', 
-     user_type: 'staff', department_ou: 'IT', faculty_code: '123'},
+     user_type: 'staff', department_ou: '064', faculty_code: '123'},
      { username: 'student1',  given_name: 'student', family_name: 'user', email_address: 'student1@example.com', 
      user_type: 'student', department_ou: 'CompSci', faculty_code: '456'},
      { username: 'archivist1',  given_name: 'archivist', family_name: 'user', email_address: 'archivist1@example.com', 
